59201a62729700b3d7cff3e033ce18459f2a42bd
Isa Lambrechts <isa.lambrechts@hotmail.be>
2018-06-27 17:19:02 +0200

    Test Polygon

--- app/src/main/java/be/ap/edu/buurtmeter/MainActivity.java
+++ app/src/main/java/be/ap/edu/buurtmeter/MainActivity.java
@@ -2,6 +2,7 @@ package be.ap.edu.buurtmeter;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Polygon;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +57,24 @@ public class MainActivity extends AppCompatActivity {
         // default = meistraat
 
         mapView.getController().setCenter(new GeoPoint(51.2164348, 4.4112339));
+
+        List<GeoPoint> geoPoints = new ArrayList<>();
+//add your points here
+        geoPoints.add(new GeoPoint(51.2164348, 4.4112339));
+        geoPoints.add(new GeoPoint(51.229857372061, 4.4242370563038));
+        geoPoints.add(new GeoPoint(51.229829165066, 4.4244103034827));
+        Polygon polygon = new Polygon();    //see note below
+        polygon.setFillColor(Color.argb(75, 255,0,0));
+        geoPoints.add(geoPoints.get(0));    //forces the loop to close
+        polygon.setPoints(geoPoints);
+        polygon.setTitle("A sample polygon");
+
+//polygons supports holes too, points should be in a counter-clockwise order
+        List<List<GeoPoint>> holes = new ArrayList<>();
+        holes.add(geoPoints);
+        polygon.setHoles(holes);
+
+        mapView.getOverlayManager().add(polygon);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
